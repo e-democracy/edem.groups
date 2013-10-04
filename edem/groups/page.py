f2606334b8d03d8753be4bfca918f7fe3378ebbb
Michael JasonSmith <mpj17@onlinegroups.net>
2013-10-04 15:30:41 -0700

    Adding a get_status method to the page-view, rather than using the barking mad groupInfo method.

---
+++ edem/groups/page.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from gs.group.stats import GroupPostingStats
+from gs.groups.page import GroupsPage  # TODO: Move all of gs.groups.page here.
+
+
+class EDemGroupsPage(GroupsPage):
+
+    def __init__(self, groups, request):
+        super(EDemGroupsPage, self).__init__(groups, request)
+        self.groups = groups
+
+    def get_stats(self, groupInfo):
+        retval = GroupPostingStats(groupInfo)
+        return retval
