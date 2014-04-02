8295bc2dc0a40c14dc888ec82bc75ac7acada3f0
William Bushey <wbushey@gmail.com>
2014-04-02 16:08:10 -0500

    Bumped version number and development status Made setup.py PEP 8

--- setup.py
+++ setup.py
@@ -1,34 +1,37 @@
 # -*- coding=utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
-setup(name='edem.groups',
+setup(
+    name='edem.groups',
     version=version,
     description="E-Democracy's customization of the groups page",
     long_description=open("README.txt").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.txt")).read(),
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers for values 
+    open(os.path.join("docs", "HISTORY.txt")).read(),
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers for values
     classifiers=[
-      "Development Status :: 1 - Planning",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
-      "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later "
+        "(GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
     keywords='',
     author='Bill Bushey',
     author_email='bill.bushey@e-democracy.org',
     url='http://www.e-democracy.org/',
     license='GPL 3',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['edem'], 
+    namespace_packages=['edem'],
     include_package_data=True,
     zip_safe=True,
     install_requires=[
@@ -45,5 +48,3 @@ setup(name='edem.groups',
     entry_points="""
     # -*- Entry points: -*-
     """,)
-
-
