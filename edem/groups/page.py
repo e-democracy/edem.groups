6b3f4befb0ca2857cf6eca44498153d78282ea91
Michael JasonSmith <mpj17@onlinegroups.net>
2014-04-08 12:35:01 -0700

    Following gs.groups.page to gs.shim.groups.page.

--- edem/groups/page.py
+++ edem/groups/page.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from gs.group.stats import GroupPostingStats
-from gs.groups.page import GroupsPage  # TODO: Move all of gs.groups.page here.
+# FIXME: Move all of gs.groups.page to edem.groups.page
+from gs.shim.groups.page import GroupsPage
 
 
 class EDemGroupsPage(GroupsPage):
